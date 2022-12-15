da9c3b6f95355d6481a295eb875ae5cc731dcb38
Fatalis <connman2400@gmail.com>
2022-12-14 19:18:57 -0600

    update route for category added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,8 +63,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const dbCategoryData = Category.update(req.body,{where:{id: req.params.id}})
+    res.status(200).json(dbCategoryData);
+  }catch(err){
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
