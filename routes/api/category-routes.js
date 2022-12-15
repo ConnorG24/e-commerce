8281d1de8673952b1810edfb72cd6b9a085fd636
Fatalis <connman2400@gmail.com>
2022-12-14 18:49:22 -0600

    category get route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,26 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const dbCategoryData = Category.findAll({
+      include: [
+        {
+          model: Product,
+          attribute: ['id','product_name','price','stock','category_id']
+        }
+      ]
+    })
+
+    if(dbCategoryData){
+      res.status(200).json(dbCategoryData);
+    }else{
+      res.status(404).json({message:'The category you are looking for does not exist'})
+    }
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
