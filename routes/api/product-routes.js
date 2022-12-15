3e2725a73af139f36bf224e4d8c88b5758bd12b6
Fatalis <connman2400@gmail.com>
2022-12-15 01:38:31 -0600

    product route find one

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,11 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try{
-    const dbProdcutData = Category.findAll({
+    const dbProdcutData = Product.findAll({
       attributes:['id','product_name','price','stock'],
       include: [
         {
@@ -34,13 +34,37 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const dbProdcutData = Product.findByPk(req.params.id,{
+      attributes:['id','product_name','price','stock'],
+      include: [
+        {
+          model: Category,
+          attribute: ['category_name']
+        },
+        {
+          model:Tag,
+          attribute:['tag_name']
+        }
+      ]
+    })
+
+    
+      res.status(200).json(dbProdcutData);
+    
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json(err)
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+  
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -49,6 +73,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+  
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
